e7522095ebe4df436d0338d1e70a1d706059a116
Laoma Nogueira <laomanogueira@gmail.com>
2021-11-18 15:33:12 -0300

    Implementada busca por palavra e melhorias

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -19,33 +19,50 @@ end
 def register_item()
   puts "Digite o título do seu item de estudo:"
   title = gets.chomp()
-  puts "#1 - Ruby\n#2 - Rails\n#3 - HTML"
-  puts "Escolha uma categoria para o seu item de estudo:"
-  option = gets.to_i()
-  study_item = StudyItem.new(title: title, option: option)
+  study_item = StudyItem.new(title: title)
   puts "Item #{study_item.title} da categoria #{study_item.category} cadastrado com sucesso!"
   study_item
 end
 
-def find_by_word()
-  puts "Digite uma palavra para procurar:"
+def find_by_word(word)
+  quantity_items = 0
+  found_items = []
+
+  @items.each_with_index do |item, index|
+    if item.title.downcase.include?(word.downcase)
+      found_items << "##{index} - #{item.title} - #{item.category}"
+      quantity_items += 1
+    end
+  end
+
+  if quantity_items == 1
+    puts "Foi encontrado #{quantity_items} item:\n"
+  elsif quantity_items > 1
+    puts "Foram encontrados #{quantity_items} itens:"
+  else
+    puts "Nenhum item encontrado com o termo: #{word}"
+  end
+  
+  found_items.each { |item| puts item}
 end
 
 puts "Bem-vindo ao Diário de Estudos, seu companheiro para estudar!"
 option = menu()
-items = []
+@items = []
 
 loop do
 
   case option
   when REGISTER_ITEM
-    items << register_item()
+    @items << register_item()
   when FIND_ALL_ITEMS
-    items.each do |item|
+    @items.each do |item|
       puts "#{item.title} - #{item.category}"
     end
   when FIND_WORD
-  
+    puts "Digite uma palavra para procurar:"
+    word = gets.chomp()
+    find_by_word(word)
   when EXIT
     break
   else
